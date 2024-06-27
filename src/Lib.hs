e930920f3ae819d3f4c44841dd1196badfa73adc
Alex Paca <fujorita772@gmail.com>
2024-06-27 16:08:54 -0400

    feat: int and insert symbol for symbol table

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,7 +7,6 @@ import Text.Parsec
 
 someFunc :: IO ()
 someFunc = do
-    --let input = "let int result = 0;\nlet bool flag = true;\nlet string message = \"Process completed\";"
     input <- readFile "program.gozu"
     case parse parseProgram "" input of
         Left err -> print err
